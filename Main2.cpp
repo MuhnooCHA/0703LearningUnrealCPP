a3d6c4e90055d1bfae0a2a21568d6db40551914e
MuhnooCHA <gjsdn1268@naver.com>
2023-07-06 17:45:29 +0900

    진짜 마지막

--- Main2.cpp
+++ Main2.cpp
@@ -10,11 +10,15 @@ int main()
 {
 	map<int, State> States;
 	States[1] = State(1, "배회");
-	States[2] = State(1, "배회");
-	States[3] = State(1, "배회");
-	States[4] = State(1, "배회");
+	States[2] = State(1, "추격");
+	States[3] = State(1, "접근");
+	States[4] = State(1, "죽음");
 
 	vector<Transitiona> Transitions;
 
 	Transitions.push_back(Transitiona(1, "적발견", 2));
+	Transitions.push_back(Transitiona(2, "적놓침", 1));
+	Transitions.push_back(Transitiona(2, "사정거리접근", 3));
+	Transitions.push_back(Transitiona(3, "사정거리이탈", 2));
+	Transitions.push_back(Transitiona(3, "HP없음", 4));
 }
